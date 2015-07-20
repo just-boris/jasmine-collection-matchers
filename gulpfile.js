746ed338f7e32cb188d93b062a396fc12407bd90
just-boris <just-boris@yandex-team.ru>
2015-07-20 11:37:22 +0300

    mirgate to jasmine-npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
+'use strict';
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     concat = require('gulp-concat'),
-    spawn = require('child_process').spawn;
+    Jasmine = require('jasmine');
 
 gulp.task('lint', function() {
     return gulp.src(['(src|test)/**'])
@@ -16,9 +17,37 @@ gulp.task('concat', function() {
         .pipe(gulp.dest('lib/'));
 });
 
+function runJasmine(config, done) {
+    var jasmine = new Jasmine(),
+        defaultConfig = {
+          spec_dir: 'test',
+          spec_files: [
+            '*[sS]pec.js'
+          ]
+        };    
+    jasmine.loadConfig(defaultConfig);
+    if(config) {
+       jasmine.loadConfig(config);
+    }
+    jasmine.onComplete(function(passed) {
+        if(passed) {
+            done();
+        } else {
+            done(new Error('some tests has been failed'));
+        }
+    });
+    jasmine.execute();
+}
+
 gulp.task('jasmine', ['lint'], function(done) {
-    var jasmine = spawn('./node_modules/.bin/jasmine-node', ['test'], {stdio: 'inherit'});
-    jasmine.on('close', done);
+    runJasmine(null, done);
+});
+
+gulp.task('revert-test', function(done) {
+    runJasmine({
+        spec_dir: 'util',
+        helpers: ['jasmineRevertSpecs.js']
+    }, done);
 });
 
 gulp.task('default', ['lint', 'concat', 'jasmine']);
--- util/jasmineRevertSpecs.js
+++ util/jasmineRevertSpecs.js
@@ -2,9 +2,9 @@ jasmine.Expectation.Factory = function(options) {
     options = options || {};
     options.isNot = true;
 
-    var expect = new Expectation(options);
+    var expect = new jasmine.Expectation(options);
     options.isNot = false;
-    expect.not = new Expectation(options);
+    expect.not = new jasmine.Expectation(options);
 
     return expect;
 };

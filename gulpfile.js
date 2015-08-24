33f06966a276b58df6329f1650dd376a5dc9e2e2
just-boris <just-boris@yandex-team.ru>
2015-08-25 00:11:17 +0300

    avoid hoisted Promise value

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,13 @@ var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     concat = require('gulp-concat'),
     Jasmine = require('jasmine'),
-    Promise = (typeof(Promise) === 'undefined') ? require('promise') : Promise;
-
-
+    Promise = typeof global.Promise === 'undefined' ? require('promise') : global.Promise;
 
 gulp.task('lint', function() {
     return gulp.src(['(src|test)/**'])
         .pipe(jshint())
         .pipe(jshint.reporter())
-        .pipe(jshint.reporter('fail'))
+        .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('concat', function() {
